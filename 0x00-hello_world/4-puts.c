32ed069842b3bdcc907104adb98948be02b59bc3
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-16 07:32:07 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main - A program that prints what is in puts function
+ * Return: 0 (Success)
+ */
+int main(void)
 {
-char Mytxt[] = "\"Programming is like building a multilingual puzzle";
-puts(Mytxt);
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
