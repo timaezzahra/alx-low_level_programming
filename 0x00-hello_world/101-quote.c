a159d1d60c759a5e046a01f605d614fe32a4696a
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-17 00:46:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
   */
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (STDOUT_FILENO, s, strlen(s));
-	return (1);
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO, s, strlen(s));
+	return (0);
 }
