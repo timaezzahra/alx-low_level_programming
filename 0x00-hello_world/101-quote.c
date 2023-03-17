c30b2d7e50e26c0223c49d4b14cf6f03de5f7808
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-17 00:39:46 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+/**
+  * main - a function to print without using printf or puts
+  * Return: 1 (Success)
+  */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write (STDOUT_FILENO, s, strlen(s));
+	return (1);
+}
