abe33c05ccfb3c0f6a65cb528415c61c714a4e9a
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-18 03:44:12 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - print alphabets in lowercase
+  *
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int Nb;
+		for (Nb = 0; Nb <= 10; Nb++)
+	{
+		printf("%d", Nb);
+	}
+		printf("\n");
+return (0);
+}
