288052b8ce93c8e523c86713363ca81f458139a0
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-18 12:37:32 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,15 @@
 int main(void)
 {
 	int nb;
-		for (nb = '0'; nb <= '8'; nb++)
+		for (nb = '0'; nb <= '9'; nb++)
 	{
-		putchar(nb);
-		putchar(',');
-		putchar(' ');
+			putchar(nb);
+		if (nb != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-		putchar('9');
 		putchar('\n');
 return (0);
 }
