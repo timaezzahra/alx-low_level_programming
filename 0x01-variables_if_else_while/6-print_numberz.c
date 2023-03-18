c6daa48b9856d05f8aa49854d0ef981239c56865
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-18 03:51:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - print alphabets in lowercase
+  *
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int nb;
+		for (nb = 0; nb < 10; nb++)
+	{
+		putchar('0' + nb);
+	}
+		putchar('\n');
+return (0);
+}
