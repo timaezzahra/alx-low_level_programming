ae4d8bb7214eb2e4102a5a1295ebad9150cee1f5
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-18 17:13:59 -0700

    ./102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - print alphabets in lowercase
+  *
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int nb;
+	int nh;
+		for (nb = 0 ; nb <= 98; nb++)
+		{
+			for (nh = nb; nh <= 99; nh++)
+			{
+				if (nh != nb)
+				{
+					putchar((nb / 10) + '0');
+					putchar((nb % 10) + '0');
+					putchar(' ');
+					putchar((nh / 10) + '0');
+					putchar((nh % 10) + '0');
+				}
+			}
+				if (nb != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+		putchar('\n');
+return (0);
+}
