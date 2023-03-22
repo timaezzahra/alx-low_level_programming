2ec9165f28dcbf95fb9ca22f20469c5a14dd5134
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-03-22 16:37:07 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - prints the first 98 fibonacci
+  * Return: 0
+  */
+int main(void)
+{
+	int count;
+	unsigned long f1 = 0, f2 = 1, sum;
+	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
+	unsigned long h1, h2;
+
+	for (count = 0; count < 92; count++)
+	{
+		sum = f1 + f2;
+		printf("%lu, ", sum);
+
+		f1 = f2;
+		f2 = sum;
+	}
+
+	f1_h1 = f1 / 10000000000;
+	f2_h1 = f2 / 10000000000;
+	f1_h2 = f1 % 10000000000;
+	f2_h2 = f2 % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		h1 = f1_h1 + f2_h1;
+		h2 = f1_h2 + f2_h2;
+		if (f1_h2 + f2_h2 > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+
+		printf("%lu%lu", h1, h2);
+		if (count != 98)
+			printf(", ");
+
+		f1_h1 = f2_h1;
+		f1_h2 = f2_h2;
+		f2_h1 = h1;
+		f2_h1 = h2;
+	}
+	printf("\n");
+	return (0);
+}
