e4372fa16faa8d923082b5d36c3d10e47a37f8cf
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-04-06 18:34:18 -0700

    ./101-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int check(char *s1, char *s2, int i, int j)
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (check(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (check(s1, s2, i + 1, j) || check(s1, s2, i, j + 1));;
+		return (check(s1, s2, i + 1, j) || check(s1, s2, i, j + 1));
 	return (0);
 }
 /**
