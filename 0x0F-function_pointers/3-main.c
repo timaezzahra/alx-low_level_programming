a245e13a970d2afe1a449be7dff61575479f40dc
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-04-21 09:10:15 -0700

    operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,11 +22,10 @@ int main(int argc, char **argv)
 
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
-		printf("Error\n"),exit(99);
+		printf("Error\n"), exit(99);
 	if (!n && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n"), exit(100);
 
 	printf("%d\n", op_func(m, n));
-	return(0);
+	return (0);
 }
-			
