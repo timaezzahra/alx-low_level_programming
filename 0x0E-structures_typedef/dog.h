a18989a3d110abdec32dd2db079f9c7b6fbf7d3f
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-04-21 16:48:25 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DIG_H
+#define DOG_H
+
+/**
+  * struct dog - struct dog
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  * Description: a lone dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
