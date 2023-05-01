3db0ccd99ee5fa63fce5c9c67108573e24687fe6
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-05-01 03:43:44 -0700

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *nw_node = malloc(sizeof(listint_t));
 
-	if (!head || nw_node)
+	if (!head || !nw_node)
 		return (NULL);
 	nw_node->next = NULL;
 	nw_node->n = n;
