3bffab716e4aa0a1e02c144e26d5674e92c1395a
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-05-01 03:29:28 -0700

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * listint_len - returns the number of elements in a linked listint_t list.
+  * @h: pointer to first node
+  * Return: number of node
+  */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
