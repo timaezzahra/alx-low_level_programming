a7fdb2bd6a0103241586767a2df12cbfddbe1678
Fatima ezzahra <fati.ouaatat@gmail.com>
2023-05-08 16:42:18 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	let = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0700);
+	let = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (let == -1)
 		return (-1);
 	if (!text_content)
